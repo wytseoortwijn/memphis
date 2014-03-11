b1acab26b94474150f60978f1088eb31f325d252
Wytse Oortwijn <wytseoortwijn@gmail.com>
2014-03-11 10:57:01 +0100

    made some changes to main.c

--- examples/rdma_test1/main.c
+++ examples/rdma_test1/main.c
@@ -1,3 +1,6 @@
+//http://www.hpcadvisorycouncil.com/pdf/building-an-rdma-capable-application-with-ib-verbs.pdf
+//http://mpi.deino.net/mpi_functions/MPI_Get_address.html
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,17 +15,20 @@ int main(int argc, char** argv) {
 	int ierr, num_procs, id, len;
 	char name[MPI_MAX_PROCESSOR_NAME];
 	MPI_Status status;
+	MPI_Aint addr;
+	int buf[2];
 
 	ierr = MPI_Init(&argc, &argv);
 	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 	ierr = MPI_Get_processor_name(name, &len);
+	ierr = MPI_Get_address(&buf[0], &addr);
 
-	printf("I am process %i of %i on %s.\n", id, num_procs, name);
+	printf("I am process %i of %i on %s. Addr: %lx \n", id, num_procs, name, (long)addr);
 
 	if (id == 0) 
 		server();
-	else 
+	else if (id == 2)
 		client();
 
 	MPI_Finalize();
